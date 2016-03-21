994b07339d88c091d510f2ee185b330e8e0439ce
Jeffrey Dunham <jeffrey.a.dunham@gmail.com>
2016-03-21 17:37:23 -0400

    Latest libcloud

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import sys
 from setuptools import setup, find_packages
 
 wargs = {}
-requires = ['DDF5Client.py', 'six', 'apache-libcloud>=1.0.0-pre1', 'click', 'requests', 'xmltodict']
+requires = ['DDF5Client.py', 'six', 'apache-libcloud', 'click', 'requests', 'xmltodict']
 
 # python 2.7 hackery
 if sys.version_info <= (3, 0):
@@ -16,7 +16,7 @@ setup(
     description="Base description, say what your package does here",
     url="https://www.dimensiondata.com/",
     name="DDDisasterRecovery.py",
-    version="0.1.2",
+    version="0.1.1",
     packages=find_packages(exclude=["contrib", "docs", "tests*", "tasks", "venv"]),
     install_requires=requires,
     scripts=['bin/dd_nsx', 'bin/dd_comparison', 'bin/dd_f5'],
